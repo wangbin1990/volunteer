8adba676f87f71dcb72cbce069744fadac54a977
wangbin <514330014@qq.com>
2018-01-18 23:34:11 +0800

    学校模块设置

---
+++ backend/web/backend/views/admin-finance/index.php
@@ -0,0 +1,415 @@
+
+<?php
+use yii\widgets\LinkPager;
+use yii\base\Object;
+use yii\bootstrap\ActiveForm;
+use common\utils\CommonFun;
+use yii\helpers\Url;
+
+use common\models\AdminFinance;
+
+$modelLabel = new \common\models\AdminFinance();
+?>
+
+<?php $this->beginBlock('header');  ?>
+<!-- <head></head>中代码块 -->
+<?php $this->endBlock(); ?>
+
+<!-- Main content -->
+<section class="content">
+  <div class="row">
+    <div class="col-xs-12">
+      <div class="box">
+      
+        <div class="box-header">
+          <h3 class="box-title">数据列表</h3>
+          <div class="box-tools">
+            <div class="input-group input-group-sm" style="width: 150px;">
+                <button id="create_btn" type="button" class="btn btn-xs btn-primary">添&nbsp;&emsp;加</button>
+        			|
+        		<button id="delete_btn" type="button" class="btn btn-xs btn-danger">批量删除</button>
+            </div>
+          </div>
+        </div>
+        <!-- /.box-header -->
+        
+        <div class="box-body">
+          <div id="example1_wrapper" class="dataTables_wrapper form-inline dt-bootstrap">
+            <!-- row start search-->
+          	<div class="row">
+          	<div class="col-sm-12">
+                <?php ActiveForm::begin(['id' => 'admin-finance-search-form', 'method'=>'get', 'options' => ['class' => 'form-inline'], 'action'=>Url::toRoute('admin-finance/index')]); ?>     
+                
+                  <div class="form-group" style="margin: 5px;">
+                      <label><?=$modelLabel->getAttributeLabel('id')?>:</label>
+                      <input type="text" class="form-control" id="query[id]" name="query[id]"  value="<?=isset($query["id"]) ? $query["id"] : "" ?>">
+                  </div>
+              <div class="form-group">
+              	<a onclick="searchAction()" class="btn btn-primary btn-sm" href="#"> <i class="glyphicon glyphicon-zoom-in icon-white"></i>搜索</a>
+           	  </div>
+               <?php ActiveForm::end(); ?> 
+            </div>
+          	</div>
+          	<!-- row end search -->
+          	
+          	<!-- row start -->
+          	<div class="row">
+          	<div class="col-sm-12">
+          	<table id="data_table" class="table table-bordered table-striped dataTable" role="grid" aria-describedby="data_table_info">
+            <thead>
+            <tr role="row">
+            
+            <?php 
+              $orderby = isset($_GET['orderby']) ? $_GET['orderby'] : '';
+		      echo '<th><input id="data_table_check" type="checkbox"></th>';
+              echo '<th onclick="orderby(\'id\', \'desc\')" '.CommonFun::sortClass($orderby, 'id').' tabindex="0" aria-controls="data_table" rowspan="1" colspan="1" aria-sort="ascending" >'.$modelLabel->getAttributeLabel('id').'</th>';
+              echo '<th onclick="orderby(\'module_id\', \'desc\')" '.CommonFun::sortClass($orderby, 'module_id').' tabindex="0" aria-controls="data_table" rowspan="1" colspan="1" aria-sort="ascending" >'.$modelLabel->getAttributeLabel('module_id').'</th>';
+              echo '<th onclick="orderby(\'status\', \'desc\')" '.CommonFun::sortClass($orderby, 'status').' tabindex="0" aria-controls="data_table" rowspan="1" colspan="1" aria-sort="ascending" >'.$modelLabel->getAttributeLabel('status').'</th>';
+              echo '<th onclick="orderby(\'fee\', \'desc\')" '.CommonFun::sortClass($orderby, 'fee').' tabindex="0" aria-controls="data_table" rowspan="1" colspan="1" aria-sort="ascending" >'.$modelLabel->getAttributeLabel('fee').'</th>';
+              echo '<th onclick="orderby(\'create_time\', \'desc\')" '.CommonFun::sortClass($orderby, 'create_time').' tabindex="0" aria-controls="data_table" rowspan="1" colspan="1" aria-sort="ascending" >'.$modelLabel->getAttributeLabel('create_time').'</th>';
+              echo '<th onclick="orderby(\'update_time\', \'desc\')" '.CommonFun::sortClass($orderby, 'update_time').' tabindex="0" aria-controls="data_table" rowspan="1" colspan="1" aria-sort="ascending" >'.$modelLabel->getAttributeLabel('update_time').'</th>';
+              echo '<th onclick="orderby(\'update_user\', \'desc\')" '.CommonFun::sortClass($orderby, 'update_user').' tabindex="0" aria-controls="data_table" rowspan="1" colspan="1" aria-sort="ascending" >'.$modelLabel->getAttributeLabel('update_user').'</th>';
+         
+			?>
+	
+            <th tabindex="0" aria-controls="data_table" rowspan="1" colspan="1" aria-sort="ascending" >操作</th>
+            </tr>
+            </thead>
+            <tbody>
+            
+            <?php
+            foreach ($models as $model) {
+                echo '<tr id="rowid_' . $model->id . '">';
+                echo '  <td><label><input type="checkbox" value="' . $model->id . '"></label></td>';
+                echo '  <td>' . $model->id . '</td>';
+                echo '  <td>' . $model->module_id . '</td>';
+                echo '  <td>' . $model->status . '</td>';
+                echo '  <td>' . $model->fee . '</td>';
+                echo '  <td>' . $model->create_time . '</td>';
+                echo '  <td>' . $model->update_time . '</td>';
+                echo '  <td>' . $model->update_user . '</td>';
+                echo '  <td class="center">';
+                echo '      <a id="view_btn" onclick="viewAction(' . $model->id . ')" class="btn btn-primary btn-sm" href="#"> <i class="glyphicon glyphicon-zoom-in icon-white"></i>查看</a>';
+                echo '      <a id="edit_btn" onclick="editAction(' . $model->id . ')" class="btn btn-primary btn-sm" href="#"> <i class="glyphicon glyphicon-edit icon-white"></i>修改</a>';
+                echo '      <a id="delete_btn" onclick="deleteAction(' . $model->id . ')" class="btn btn-danger btn-sm" href="#"> <i class="glyphicon glyphicon-trash icon-white"></i>删除</a>';
+                echo '  </td>';
+                echo '</tr>';
+            }
+            
+            ?>
+            
+           
+           
+            </tbody>
+            <!-- <tfoot></tfoot> -->
+          </table>
+          </div>
+          </div>
+          <!-- row end -->
+          
+          <!-- row start -->
+          <div class="row">
+          	<div class="col-sm-5">
+            	<div class="dataTables_info" id="data_table_info" role="status" aria-live="polite">
+            		<div class="infos">
+            		从<?= $pages->getPage() * $pages->getPageSize() + 1 ?>            		到 <?= ($pageCount = ($pages->getPage() + 1) * $pages->getPageSize()) < $pages->totalCount ?  $pageCount : $pages->totalCount?>            		 共 <?= $pages->totalCount?> 条记录</div>
+            	</div>
+            </div>
+          	<div class="col-sm-7">
+              	<div class="dataTables_paginate paging_simple_numbers" id="data_table_paginate">
+              	<?= LinkPager::widget([
+              	    'pagination' => $pages,
+              	    'nextPageLabel' => '»',
+              	    'prevPageLabel' => '«',
+              	    'firstPageLabel' => '首页',
+              	    'lastPageLabel' => '尾页',
+              	]); ?>	
+              	
+              	</div>
+          	</div>
+		  </div>
+		  <!-- row end -->
+        </div>
+        </div>
+        <!-- /.box-body -->
+      </div>
+      <!-- /.box -->
+    </div>
+    <!-- /.col -->
+  </div>
+  <!-- /.row -->
+</section>
+<!-- /.content -->
+
+<div class="modal fade" id="edit_dialog" tabindex="-1" role="dialog"
+	aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">×</button>
+				<h3>Settings</h3>
+			</div>
+			<div class="modal-body">
+                <?php $form = ActiveForm::begin(["id" => "admin-finance-form", "class"=>"form-horizontal", "action"=>Url::toRoute("admin-finance/save")]); ?>                      
+                 
+          <input type="hidden" class="form-control" id="id" name="id" />
+
+          <div id="module_id_div" class="form-group">
+              <label for="module_id" class="col-sm-2 control-label"><?php echo $modelLabel->getAttributeLabel("module_id")?></label>
+              <div class="col-sm-10">
+                  <input type="text" class="form-control" id="module_id" name="AdminFinance[module_id]" placeholder="必填" />
+              </div>
+              <div class="clearfix"></div>
+          </div>
+
+          <div id="status_div" class="form-group">
+              <label for="status" class="col-sm-2 control-label"><?php echo $modelLabel->getAttributeLabel("status")?></label>
+              <div class="col-sm-10">
+                  <input type="text" class="form-control" id="status" name="AdminFinance[status]" placeholder="必填" />
+              </div>
+              <div class="clearfix"></div>
+          </div>
+
+          <div id="fee_div" class="form-group">
+              <label for="fee" class="col-sm-2 control-label"><?php echo $modelLabel->getAttributeLabel("fee")?></label>
+              <div class="col-sm-10">
+                  <input type="text" class="form-control" id="fee" name="AdminFinance[fee]" placeholder="必填" />
+              </div>
+              <div class="clearfix"></div>
+          </div>
+
+          <div id="create_time_div" class="form-group">
+              <label for="create_time" class="col-sm-2 control-label"><?php echo $modelLabel->getAttributeLabel("create_time")?></label>
+              <div class="col-sm-10">
+                  <input type="text" class="form-control" id="create_time" name="AdminFinance[create_time]" placeholder="必填" />
+              </div>
+              <div class="clearfix"></div>
+          </div>
+
+          <div id="update_time_div" class="form-group">
+              <label for="update_time" class="col-sm-2 control-label"><?php echo $modelLabel->getAttributeLabel("update_time")?></label>
+              <div class="col-sm-10">
+                  <input type="text" class="form-control" id="update_time" name="AdminFinance[update_time]" placeholder="必填" />
+              </div>
+              <div class="clearfix"></div>
+          </div>
+
+          <div id="update_user_div" class="form-group">
+              <label for="update_user" class="col-sm-2 control-label"><?php echo $modelLabel->getAttributeLabel("update_user")?></label>
+              <div class="col-sm-10">
+                  <input type="text" class="form-control" id="update_user" name="AdminFinance[update_user]" placeholder="必填" />
+              </div>
+              <div class="clearfix"></div>
+          </div>
+                    
+
+			<?php ActiveForm::end(); ?>          
+                </div>
+			<div class="modal-footer">
+				<a href="#" class="btn btn-default" data-dismiss="modal">关闭</a> <a
+					id="edit_dialog_ok" href="#" class="btn btn-primary">确定</a>
+			</div>
+		</div>
+	</div>
+</div>
+<?php $this->beginBlock('footer');  ?>
+<!-- <body></body>后代码块 -->
+ <script>
+function orderby(field, op){
+	 var url = window.location.search;
+	 var optemp = field + " desc";
+	 if(url.indexOf('orderby') != -1){
+		 url = url.replace(/orderby=([^&?]*)/ig,  function($0, $1){ 
+			 var optemp = field + " desc";
+			 optemp = decodeURI($1) != optemp ? optemp : field + " asc";
+			 return "orderby=" + optemp;
+		   }); 
+	 }
+	 else{
+		 if(url.indexOf('?') != -1){
+			 url = url + "&orderby=" + encodeURI(optemp);
+		 }
+		 else{
+			 url = url + "?orderby=" + encodeURI(optemp);
+		 }
+	 }
+	 window.location.href=url; 
+ }
+ function searchAction(){
+		$('#admin-finance-search-form').submit();
+	}
+ function viewAction(id){
+		initModel(id, 'view', 'fun');
+	}
+
+ function initEditSystemModule(data, type){
+	if(type == 'create'){
+		$("#id").val('');
+		$("#module_id").val('');
+		$("#status").val('');
+		$("#fee").val('');
+		$("#create_time").val('');
+		$("#update_time").val('');
+		$("#update_user").val('');
+		
+	}
+	else{
+		$("#id").val(data.id);
+    	$("#module_id").val(data.module_id);
+    	$("#status").val(data.status);
+    	$("#fee").val(data.fee);
+    	$("#create_time").val(data.create_time);
+    	$("#update_time").val(data.update_time);
+    	$("#update_user").val(data.update_user);
+    	}
+	if(type == "view"){
+      $("#id").attr({readonly:true,disabled:true});
+      $("#module_id").attr({readonly:true,disabled:true});
+      $("#status").attr({readonly:true,disabled:true});
+      $("#fee").attr({readonly:true,disabled:true});
+      $("#create_time").attr({readonly:true,disabled:true});
+      $("#update_time").attr({readonly:true,disabled:true});
+      $("#update_user").attr({readonly:true,disabled:true});
+	$('#edit_dialog_ok').addClass('hidden');
+	}
+	else{
+      $("#id").attr({readonly:false,disabled:false});
+      $("#module_id").attr({readonly:false,disabled:false});
+      $("#status").attr({readonly:false,disabled:false});
+      $("#fee").attr({readonly:false,disabled:false});
+      $("#create_time").attr({readonly:false,disabled:false});
+      $("#update_time").attr({readonly:false,disabled:false});
+      $("#update_user").attr({readonly:false,disabled:false});
+		$('#edit_dialog_ok').removeClass('hidden');
+		}
+		$('#edit_dialog').modal('show');
+}
+
+function initModel(id, type, fun){
+	
+	$.ajax({
+		   type: "GET",
+		   url: "<?=Url::toRoute('admin-finance/view')?>",
+		   data: {"id":id},
+		   cache: false,
+		   dataType:"json",
+		   error: function (xmlHttpRequest, textStatus, errorThrown) {
+			    alert("出错了，" + textStatus);
+			},
+		   success: function(data){
+			   initEditSystemModule(data, type);
+		   }
+		});
+}
+	
+function editAction(id){
+	initModel(id, 'edit');
+}
+
+function deleteAction(id){
+	var ids = [];
+	if(!!id == true){
+		ids[0] = id;
+	}
+	else{
+		var checkboxs = $('#data_table :checked');
+	    if(checkboxs.size() > 0){
+	        var c = 0;
+	        for(i = 0; i < checkboxs.size(); i++){
+	            var id = checkboxs.eq(i).val();
+	            if(id != ""){
+	            	ids[c++] = id;
+	            }
+	        }
+	    }
+	}
+	if(ids.length > 0){
+		admin_tool.confirm('请确认是否删除', function(){
+		    $.ajax({
+				   type: "GET",
+				   url: "<?=Url::toRoute('admin-finance/delete')?>",
+				   data: {"ids":ids},
+				   cache: false,
+				   dataType:"json",
+				   error: function (xmlHttpRequest, textStatus, errorThrown) {
+					    admin_tool.alert('msg_info', '出错了，' + textStatus, 'warning');
+					},
+				   success: function(data){
+					   for(i = 0; i < ids.length; i++){
+						   $('#rowid_' + ids[i]).remove();
+					   }
+					   admin_tool.alert('msg_info', '删除成功', 'success');
+					   window.location.reload();
+				   }
+				});
+		});
+	}
+	else{
+		admin_tool.alert('msg_info', '请先选择要删除的数据', 'warning');
+	}
+    
+}
+
+function getSelectedIdValues(formId)
+{
+	var value="";
+	$( formId + " :checked").each(function(i)
+	{
+		if(!this.checked)
+		{
+			return true;
+		}
+		value += this.value;
+		if(i != $("input[name='id']").size()-1)
+		{
+			value += ",";
+		}
+	 });
+	return value;
+}
+
+$('#edit_dialog_ok').click(function (e) {
+    e.preventDefault();
+	$('#admin-finance-form').submit();
+});
+
+$('#create_btn').click(function (e) {
+    e.preventDefault();
+    initEditSystemModule({}, 'create');
+});
+
+$('#delete_btn').click(function (e) {
+    e.preventDefault();
+    deleteAction('');
+});
+
+$('#admin-finance-form').bind('submit', function(e) {
+	e.preventDefault();
+	var id = $("#id").val();
+	var action = id == "" ? "<?=Url::toRoute('admin-finance/create')?>" : "<?=Url::toRoute('admin-finance/update')?>";
+    $(this).ajaxSubmit({
+    	type: "post",
+    	dataType:"json",
+    	url: action,
+    	success: function(value) 
+    	{
+        	if(value.errno == 0){
+        		$('#edit_dialog').modal('hide');
+        		admin_tool.alert('msg_info', '添加成功', 'success');
+        		window.location.reload();
+        	}
+        	else{
+            	var json = value.data;
+        		for(var key in json){
+        			$('#' + key).attr({'data-placement':'bottom', 'data-content':json[key], 'data-toggle':'popover'}).addClass('popover-show').popover('show');
+        			
+        		}
+        	}
+
+    	}
+    });
+});
+
+ 
+</script>
+<?php $this->endBlock(); ?>
---
+++ common/models/AdminFinance.php
@@ -0,0 +1,241 @@
+<?php
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%admin_charge_module}}".
+ *
+ * @property integer $id
+ * @property integer $module_id
+ * @property integer $status
+ * @property string $fee
+ * @property integer $create_time
+ * @property integer $update_time
+ * @property string $update_user
+ */
+class AdminFinance extends \backend\models\BaseModel
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'admin_finance';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['module_id', 'status', 'create_time', 'update_time'], 'integer'],
+            [['module_id', 'status', 'fee'], 'required'],
+            [['fee'], 'number'],
+            [['module_id'], 'compare', 'compareValue' => 1, 'operator' => '>=', 'message' => '请选择数据'],
+            ['module_id', 'unique', 'message'=>'{attribute}已经配置数据了'],
+            [['update_user'], 'string', 'max' => 128]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'module_id' => '模块名称',
+            'status' => '状态',
+            'fee' => '收费标准',
+            'create_time' => '创建时间',
+            'update_time' => '修改时间',
+            'update_user' => '修改人',
+        ];
+    }
+
+
+    /**
+     * 返回数据库字段信息，仅在生成CRUD时使用，如不需要生成CRUD，请注释或删除该getTableColumnInfo()代码
+     * COLUMN_COMMENT可用key如下:
+     * label - 显示的label
+     * inputType 控件类型, 暂时只支持text,hidden  // select,checkbox,radio,file,password,
+     * isEdit   是否允许编辑，如果允许编辑将在添加和修改时输入
+     * isSearch 是否允许搜索
+     * isDisplay 是否在列表中显示
+     * isOrder 是否排序
+     * udc - udc code，inputtype为select,checkbox,radio三个值时用到。
+     * 特别字段：
+     * id：主键。必须含有主键，统一都是id
+     * create_date: 创建时间。生成的代码自动赋值
+     * update_date: 修改时间。生成的代码自动赋值
+     */
+    public function getTableColumnInfo(){
+        return array(
+        'id' => array(
+                        'name' => 'id',
+                        'allowNull' => false,
+//                         'autoIncrement' => true,
+//                         'comment' => 'id',
+//                         'dbType' => "smallint(10) unsigned",
+                        'defaultValue' => '',
+                        'enumValues' => null,
+                        'isPrimaryKey' => true,
+                        'phpType' => 'integer',
+                        'precision' => '10',
+                        'scale' => '',
+                        'size' => '10',
+                        'type' => 'smallint',
+                        'unsigned' => true,
+                        'label'=>$this->getAttributeLabel('id'),
+                        'inputType' => 'hidden',
+                        'isEdit' => true,
+                        'isSearch' => true,
+                        'isDisplay' => true,
+                        'isSort' => true,
+//                         'udc'=>'',
+                    ),
+		'module_id' => array(
+                        'name' => 'module_id',
+                        'allowNull' => false,
+//                         'autoIncrement' => false,
+//                         'comment' => '收费模块id',
+//                         'dbType' => "tinyint(3) unsigned",
+                        'defaultValue' => '0',
+                        'enumValues' => null,
+                        'isPrimaryKey' => false,
+                        'phpType' => 'integer',
+                        'precision' => '3',
+                        'scale' => '',
+                        'size' => '3',
+                        'type' => 'smallint',
+                        'unsigned' => true,
+                        'label'=>$this->getAttributeLabel('module_id'),
+                        'inputType' => 'text',
+                        'isEdit' => true,
+                        'isSearch' => false,
+                        'isDisplay' => true,
+                        'isSort' => true,
+//                         'udc'=>'',
+                    ),
+		'status' => array(
+                        'name' => 'status',
+                        'allowNull' => false,
+//                         'autoIncrement' => false,
+//                         'comment' => '开启状态1：是，0否',
+//                         'dbType' => "tinyint(3) unsigned",
+                        'defaultValue' => '0',
+                        'enumValues' => null,
+                        'isPrimaryKey' => false,
+                        'phpType' => 'integer',
+                        'precision' => '3',
+                        'scale' => '',
+                        'size' => '3',
+                        'type' => 'smallint',
+                        'unsigned' => true,
+                        'label'=>$this->getAttributeLabel('status'),
+                        'inputType' => 'text',
+                        'isEdit' => true,
+                        'isSearch' => false,
+                        'isDisplay' => true,
+                        'isSort' => true,
+//                         'udc'=>'',
+                    ),
+		'fee' => array(
+                        'name' => 'fee',
+                        'allowNull' => false,
+//                         'autoIncrement' => false,
+//                         'comment' => '收费标准',
+//                         'dbType' => "decimal(10,0)",
+                        'defaultValue' => '0',
+                        'enumValues' => null,
+                        'isPrimaryKey' => false,
+                        'phpType' => 'string',
+                        'precision' => '10',
+                        'scale' => '0',
+                        'size' => '10',
+                        'type' => 'decimal',
+                        'unsigned' => false,
+                        'label'=>$this->getAttributeLabel('fee'),
+                        'inputType' => 'text',
+                        'isEdit' => true,
+                        'isSearch' => false,
+                        'isDisplay' => true,
+                        'isSort' => true,
+//                         'udc'=>'',
+                    ),
+		'create_time' => array(
+                        'name' => 'create_time',
+                        'allowNull' => false,
+//                         'autoIncrement' => false,
+//                         'comment' => '创建时间',
+//                         'dbType' => "tinyint(10) unsigned",
+                        'defaultValue' => '0',
+                        'enumValues' => null,
+                        'isPrimaryKey' => false,
+                        'phpType' => 'integer',
+                        'precision' => '10',
+                        'scale' => '',
+                        'size' => '10',
+                        'type' => 'smallint',
+                        'unsigned' => true,
+                        'label'=>$this->getAttributeLabel('create_time'),
+                        'inputType' => 'text',
+                        'isEdit' => true,
+                        'isSearch' => false,
+                        'isDisplay' => true,
+                        'isSort' => true,
+//                         'udc'=>'',
+                    ),
+		'update_time' => array(
+                        'name' => 'update_time',
+                        'allowNull' => false,
+//                         'autoIncrement' => false,
+//                         'comment' => '修改时间',
+//                         'dbType' => "tinyint(10) unsigned",
+                        'defaultValue' => '0',
+                        'enumValues' => null,
+                        'isPrimaryKey' => false,
+                        'phpType' => 'integer',
+                        'precision' => '10',
+                        'scale' => '',
+                        'size' => '10',
+                        'type' => 'smallint',
+                        'unsigned' => true,
+                        'label'=>$this->getAttributeLabel('update_time'),
+                        'inputType' => 'text',
+                        'isEdit' => true,
+                        'isSearch' => false,
+                        'isDisplay' => true,
+                        'isSort' => true,
+//                         'udc'=>'',
+                    ),
+		'update_user' => array(
+                        'name' => 'update_user',
+                        'allowNull' => false,
+//                         'autoIncrement' => false,
+//                         'comment' => '修改人',
+//                         'dbType' => "varchar(128)",
+                        'defaultValue' => '',
+                        'enumValues' => null,
+                        'isPrimaryKey' => false,
+                        'phpType' => 'string',
+                        'precision' => '128',
+                        'scale' => '',
+                        'size' => '128',
+                        'type' => 'string',
+                        'unsigned' => false,
+                        'label'=>$this->getAttributeLabel('update_user'),
+                        'inputType' => 'text',
+                        'isEdit' => true,
+                        'isSearch' => false,
+                        'isDisplay' => true,
+                        'isSort' => true,
+//                         'udc'=>'',
+                    ),
+		        );
+        
+    }
+ 
+}
