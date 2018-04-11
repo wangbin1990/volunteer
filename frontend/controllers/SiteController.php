141fae768c1fc4d62bb3d8ee33a889bcfff01fac
wangbin <514330014@qq.com>
2018-04-11 23:10:27 +0800

    终极版本

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -844,7 +844,7 @@ class SiteController extends Controller
             $max_diff =  $diff_score - ($data['item'] - 1) * ($step - 1);
             $min_diff = $diff_score - ($data['item']) * ($step - 1);
             if ($max_diff <= 0) {
-                $offset = ($data['item'] - 1) * 4;
+                $offset = abs(floor($max_diff / $step)) * 8;
             }
 
         } elseif ($data['type'] == 2) {
@@ -861,6 +861,11 @@ class SiteController extends Controller
         } elseif ($data['type'] == 3) {
             $max_diff = -1;
             $min_diff = -2;
+            $data['year'] = [
+                $data['year'],
+                $data['year'] - 1,
+                $data['year'] - 2,
+            ];
             $offset = ($data['item'] - 1) * 8;
         }
 
